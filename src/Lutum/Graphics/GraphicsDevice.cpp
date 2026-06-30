b79f2490ca68fd6bddb5317a5db230a07e32f52f
Collin Longoria <collin.longoria@digipen.edu>
2026-06-30 13:21:55 -0700

    added but did not impl Graphics Device class

---
+++ src/Lutum/Graphics/GraphicsDevice.cpp
@@ -0,0 +1,16 @@
+/*
+* File: GraphicsDevice.cpp
+* Project: LutumEngine
+* Author: Collin
+* Created on: 6/30/2026
+*
+* Copyright (c) 2026 Collin Longoria
+*
+* This software is released under the MIT License.
+* https://opensource.org/licenses/MIT
+*/
+
+#include "GraphicsDevice.hpp"
+
+namespace Lutum {
+} // Lutum
---
+++ src/Lutum/Graphics/GraphicsDevice.hpp
@@ -0,0 +1,23 @@
+/*
+* File: GraphicsDevice.hpp
+* Project: LutumEngine
+* Author: Collin
+* Created on: 6/30/2026
+*
+* Copyright (c) 2026 Collin Longoria
+*
+* This software is released under the MIT License.
+* https://opensource.org/licenses/MIT
+*/
+
+#ifndef LUTUM_GRAPHICSDEVICE_HPP
+#define LUTUM_GRAPHICSDEVICE_HPP
+
+namespace Lutum {
+class GraphicsDevice {
+public:
+    GraphicsDevice();
+};
+} // Lutum
+
+#endif //LUTUM_GRAPHICSDEVICE_HPP
